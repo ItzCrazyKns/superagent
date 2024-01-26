8ecfa12ec2c11b8b94518cfc7d6e09912e0b748f
ItzCrazyKns <95534749+ItzCrazyKns@users.noreply.github.com>
2024-01-26 21:10:45 +0530

    Make `vector-databases` page responsive

--- libs/ui/app/vector-databases/client-page.tsx
+++ libs/ui/app/vector-databases/client-page.tsx
@@ -95,7 +95,7 @@ export default function VectorDbClientPage({
   )
 
   return (
-    <div className="grid grid-cols-5 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
       {siteConfig.vectorDbs.map((vectorDb) => (
         <VectorDbProviderCard
           api_key={profile?.api_key}
